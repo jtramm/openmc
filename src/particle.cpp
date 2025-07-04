0a1c8cefcdec59f6df65c8acaa54034623b955ce
John Tramm <john.tramm@gmail.com>
2025-07-04 15:29:49 -0500

    fixed n_event increment problem

--- src/particle.cpp
+++ src/particle.cpp
@@ -464,13 +464,7 @@ void Particle::event_collide()
 
 void Particle::event_revive_from_secondary()
 {
-  // If particle has too many events, display warning and kill it
-  ++n_event();
-  if (n_event() == settings::max_particle_events) {
-    warning("Particle " + std::to_string(id()) +
-            " underwent maximum number of events.");
-    wgt() = 0.0;
-  }
+
 
   // Check for secondary particles if this particle is dead
   if (!alive()) {
