9f1bf79af0e2e9a0291e89afa4794997bc5b6271
John Tramm <john.tramm@gmail.com>
2025-11-06 11:34:30 -0600

    added rollback of last DAGMC intersection before buffering

--- src/random_ray/random_ray.cpp
+++ src/random_ray/random_ray.cpp
@@ -486,6 +486,7 @@ void RandomRay::attenuate_flux(double distance, double offset)
     Position position_buffer =  r() + (offset + mesh_partial_length) * u();
     double distance_buffer = distance_travelled_ + mesh_partial_length;
     // double distance_buffer = distance_travelled_ + offset + mesh_partial_length;
+    history().rollback_last_intersection();
     pack_ray_for_buffer(distance_buffer, position_buffer); //, SourceRegionKey(sr, mesh_bin), sr);
     wgt() = 0.0;
     // if (id() == 6543){
